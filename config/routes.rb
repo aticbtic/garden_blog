84ff0e636229dacccea6e698ca09e14e65375093
aticbtic <angela.i.tan@gmail.com>
2020-10-17 05:49:46 +0000

    add home and modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'flowers/index'
+  get 'flowers/index', to: "flowers#index"
 #  get 'pages/home'
-root 'pages#home'
+  root 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
